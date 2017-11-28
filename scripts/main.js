1e464a1eaabb85f49efe0418c2a4a3509e09e425
Bartosz Lech <bartosz.projects1@gmail.com>
2017-11-28 15:44:55 +0100

    basic get data from json

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
 
-var hangman = angular.module('hangmanApp', []);
+const hangman = angular.module('hangmanApp', []);
 
-hangman.controller('keyboardController', function($scope) {
+hangman.service('getDataService', function ($http) {
+    this.jsonData = function() {
+        return $http.get('scripts/data.json')
+    }
+});
+
+hangman.controller('keyboardController', ($scope, $http, getDataService) =>{
     $scope.keys = 'abcdefghijklmnoprstuwvxyz'.split('');
 
     $scope.pressKey = function(clickedKey) {
         document.getElementById('singleKey').innerHTML = clickedKey;
-    }
+    };
+    
+
+    getDataService.jsonData().then((response) => $scope.words = response.data);
+
 });
