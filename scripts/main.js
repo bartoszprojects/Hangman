a3514f1ea048f557e4209deca5bcbfec118f33aa
Bartosz Lech <bartosz.projects1@gmail.com>
2017-11-28 15:13:33 +0100

    creating keyboard for game

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
 
 var hangman = angular.module('hangmanApp', []);
+
+hangman.controller('keyboardController', function($scope) {
+    $scope.keys = 'abcdefghijklmnoprstuwvxyz'.split('');
+
+    $scope.pressKey = function(clickedKey) {
+        document.getElementById('singleKey').innerHTML = clickedKey;
+    }
+});
