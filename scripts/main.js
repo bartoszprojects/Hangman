e095e1bc2f339bd19b072e8b242136c7a85c5c35
Bartosz Lech <bartosz.projects1@gmail.com>
2017-11-28 20:54:33 +0100

    all needen scripts are done

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ hangman.service('getDataService', function ($http) {
 hangman.controller('keyboardController', ($scope, $http, getDataService) => {
 
     $scope.keys = 'abcdefghijklmnoprstuwvxyz'.split('');
+    $scope.imgUrl = 1;
 
     let jsonData = function () {
         return $http.get('scripts/data.json');
@@ -20,9 +21,7 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
         $scope.words = response.data.statements;
         $scope.promptList = [];
         $scope.answerList = [];
-        $scope.error = 0;
-        $scope.subpoint = 0;
-        $scope.imgUrl = 0;
+        $scope.mainPoint = -1;
 
         let json_length = Object.keys($scope.words).length;
 
@@ -32,7 +31,50 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
             $scope.answerList.push(value['answer']);
         }
 
-        let randomNumber = Math.floor((Math.random() * $scope.answerList.length));
+        console.log($scope.promptList);
+        console.log($scope.answerList);
+
+        $scope.nextGame = function () {
+
+            $scope.promptList.splice($scope.randomNumber, 1);
+            $scope.answerList.splice($scope.randomNumber, 1);
+            $scope.randomNumber = Math.floor((Math.random() * $scope.answerList.length));
+
+            $scope.currentWord = $scope.answerList[$scope.randomNumber];
+            $scope.currentPrompt = $scope.promptList[$scope.randomNumber];
+            $scope.splittedWord = $scope.currentWord.split('');
+            $scope.hiddenWord = $scope.currentWord.replace(/\S/gi, '*');
+            $scope.returnedWord = $scope.hiddenWord.split('');
+            $scope.ultimateWord = $scope.returnedWord.join('');
+
+            $scope.subpoint = 0;
+            $scope.error = 0;
+            $scope.imgUrl = 1;
+            $scope.mainPoint += 1;
+
+            $scope.resetHTML()
+        };
+
+        $scope.resetHTML = function () {
+            $scope.keys.forEach((value) => {
+                let tempBtn = document.getElementById('button' + value);
+                tempBtn.disabled = false;
+                tempBtn.style.border = '1px solid gray';
+                console.log(value)
+            })
+        };
+
+        $scope.$watch('ultimateWord', () => {
+            $scope.ultimateWord === $scope.currentWord ? $scope.nextGame() : console.log('bad')
+
+        });
+
+        $scope.$watch('imgUrl', () => {
+            $scope.imgUrl >= 7 ? $scope.nextGame() : console.log('still good')
+        });
+
+
+        $scope.nextGame();
 
         function check(myList, searchedElement) {
             let elements = [];
@@ -43,36 +85,29 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
             }
             return elements;
         }
-        $scope.currentWord = $scope.answerList[randomNumber];
-        $scope.currentPrompt = $scope.promptList[randomNumber];
-        $scope.splittedWord = $scope.currentWord.split('');
-        $scope.hiddenWord = $scope.currentWord.replace(/\S/gi, '*');
-        $scope.returnedWord = $scope.hiddenWord.split('');
-        $scope.ultimateWord = $scope.returnedWord.join('');
 
         $scope.pressKey = function (myVar) {
-            let correct = false;
             let tempBtn = document.getElementById('button' + myVar);
             tempBtn.disabled = true;
+            let correct = false;
             let returnedIndex = check($scope.splittedWord, myVar);
             console.log(returnedIndex);
 
-            $scope.splittedWord.forEach((val, index) => {
+            $scope.splittedWord.forEach((val) => {
                 if (val === myVar) {
                     correct = true;
                     tempBtn.style.border = '1px solid green';
-
                     returnedIndex.forEach((value) => {
                         $scope.returnedWord[value] = myVar
                     });
                 }
                 $scope.ultimateWord = $scope.returnedWord.join('');
-                console.log(val);
-                console.log(index)
+
             });
             correct === true ? $scope.subpoint += 1 : (($scope.error += 1) && ($scope.imgUrl += 1) &&
-            (tempBtn.style.border = '1px solid red'));
-        }
+                (tempBtn.style.border = '1px solid red'));
+        };
+
     }
 
 });
