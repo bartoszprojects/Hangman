8e4032c5b21f0164391054d4720eb82c6b66e062
Bartosz Lech <bartosz.projects1@gmail.com>
2017-11-28 18:09:57 +0100

    points for good answer, errors, css err/good design

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,6 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
 
     $scope.keys = 'abcdefghijklmnoprstuwvxyz'.split('');
 
-
     let jsonData = function () {
         return $http.get('scripts/data.json');
     };
@@ -21,25 +20,19 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
         $scope.words = response.data.statements;
         $scope.promptList = [];
         $scope.answerList = [];
-        let json_length = Object.keys($scope.words).length;
+        $scope.error = 0;
+        $scope.subpoint = 0;
+        $scope.imgUrl = 0;
 
+        let json_length = Object.keys($scope.words).length;
 
         for (let key in $scope.words) {
             let value = $scope.words[key];
-            console.log(key, value);
-            console.log(value['answer']);
             $scope.promptList.push(value['prompt']);
             $scope.answerList.push(value['answer']);
-            console.log($scope.promptList);
-            console.log($scope.answerList);
         }
 
         let randomNumber = Math.floor((Math.random() * $scope.answerList.length));
-        console.log('----------------------');
-        console.log('random numbers: ', randomNumber, 1);
-        console.log('!!!! ', $scope.promptList[randomNumber]);
-        let splittedPromptList = $scope.promptList.splice(randomNumber, 1);
-        console.log($scope.promptList)
 
         function check(myList, searchedElement) {
             let elements = [];
@@ -50,37 +43,36 @@ hangman.controller('keyboardController', ($scope, $http, getDataService) => {
             }
             return elements;
         }
-
-        $scope.currentWord = $scope.promptList[randomNumber];
+        $scope.currentWord = $scope.answerList[randomNumber];
+        $scope.currentPrompt = $scope.promptList[randomNumber];
         $scope.splittedWord = $scope.currentWord.split('');
         $scope.hiddenWord = $scope.currentWord.replace(/\S/gi, '*');
         $scope.returnedWord = $scope.hiddenWord.split('');
         $scope.ultimateWord = $scope.returnedWord.join('');
 
         $scope.pressKey = function (myVar) {
+            let correct = false;
+            let tempBtn = document.getElementById('button' + myVar);
+            tempBtn.disabled = true;
             let returnedIndex = check($scope.splittedWord, myVar);
             console.log(returnedIndex);
 
             $scope.splittedWord.forEach((val, index) => {
-
                 if (val === myVar) {
-                    $scope.correct = true;
-                    console.log('matched !');
-
+                    correct = true;
+                    tempBtn.style.border = '1px solid green';
 
                     returnedIndex.forEach((value) => {
-
                         $scope.returnedWord[value] = myVar
                     });
                 }
-
                 $scope.ultimateWord = $scope.returnedWord.join('');
                 console.log(val);
                 console.log(index)
             });
+            correct === true ? $scope.subpoint += 1 : (($scope.error += 1) && ($scope.imgUrl += 1) &&
+            (tempBtn.style.border = '1px solid red'));
         }
-
     }
-
 
 });
